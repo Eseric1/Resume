822ade2d3f4aec050bb4c6315701d637c55c0c2b
eric samano <ericsamano48@gmail.com>
2023-05-21 12:55:08 -0700

    added pdf file

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ def show_pdf(file_path):
         base64_pdf = base64.b64encode(f.read()).decode("utf-8")
         pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
         return pdf_display
+
+st.components.v1.html(show_pdf("01-Erics-Resume.pdf"))
