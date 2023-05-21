8eefed64431809ec69da097bd58c0ebd55a41f17
eric samano <ericsamano48@gmail.com>
2023-05-21 12:44:06 -0700

    reworked streamlit compatability

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
-import base64
 import streamlit as st
 
 def show_pdf(file_path):
     with open(file_path, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-        pdf_display = f'<iframe src="01-Erics-Resume.pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
         return pdf_display
-    
-st.markdown(show_pdf("01-Erics-Resume.pdf"), unsafe_allow_html=True)
